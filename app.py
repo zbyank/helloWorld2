c429dd51e7d184d0452eab2bf71e3ccd06230f8f
zbyank <zbyank@terpmail.umd.edu>
2026-02-04 17:07:51 -0500

    Added new route for HTML page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World From Zachary Byank! I am adding my first code change.'
+
+@app.route('/hello')
+def hello():  # put application's code here
+    return render_template('hello.html')
 
 
 if __name__ == '__main__':
