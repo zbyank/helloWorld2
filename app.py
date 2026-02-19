0f5f30d97305284f309cce830b631acd5c53237e
zbyank <zbyank@terpmail.umd.edu>
2026-02-19 12:05:43 -0500

    Created a route to my hello page

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ def hello_world():  # put application's code here
 def hello():  # put application's code here
     return render_template('hello.html')
 
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
