a3581b525fe08f958902b5a17d9050c7489c6e87
zbyank <zbyank@terpmail.umd.edu>
2026-03-13 15:59:53 -0400

    added favorite-course and contact routes with base template inheritance

--- app.py
+++ app.py
@@ -1,24 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World From Zachary Byank! I am adding my first code change.'
 
 @app.route('/hello')
-def hello():  # put application's code here
+def hello():
     return render_template('hello.html')
 
 @app.route('/about')
-def about():  # put application's code here
+def about():
     return render_template('about.html')
 
-# New route for about-css page
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    subject = request.args.get('subject', '')
+    course_number = request.args.get('course_number', '')
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        return render_template('contact.html', submitted=True,
+                               first_name=first_name, last_name=last_name,
+                               email=email, message=message)
+    return render_template('contact.html', submitted=False)
 
 if __name__ == '__main__':
     app.run()
